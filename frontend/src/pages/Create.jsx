a7a67215b47944c44556e0de4253a49caac8a0a3
Ainesh Sridhar <aineshsridhar@gmail.com>
2025-04-18 23:43:53 +0530

    Updated dashboard content limit per blog container

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -6,13 +6,14 @@ const Create = () => {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const [category, setCategory] = useState("");
+    const [about, setAbout] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = async(e) => {
         e.preventDefault();
         try{
             await axios.post('http://localhost:7000/api/blogs/add-post', 
-                { title, content, category },
+                { title, about, content, category },
                 {
                     headers:{
                         Authorization: `Bearer ${localStorage.getItem('token')}`
@@ -30,24 +31,26 @@ const Create = () => {
 
   return (
     <div className="px-40 pt-10 border text-xl">
-        <h3>Speak your thoughts!</h3>
+        <h1 className="text-3xl font-bold mb-2">Speak your thoughts!</h1>
         <form onSubmit={handleSubmit} className="flex flex-col text-left">
             <div className="my-4">
                 <label htmlFor="title" className="">Title: </label>
                 <input type="text" name="title" value={title} className="px-2 py-1 w-100" placeholder="Enter title..." onChange={(e) => setTitle(e.target.value)}></input>
             </div>
-            <div className="mb-4">
-                <label htmlFor="category" className="">Category: </label>
+            <div className="mb-5">
+                <label htmlFor="category" className="mb-1">Category: </label>
                 <select value={category} onChange={(e) => setCategory(e.target.value)} className="text-gray-500 text-xl">
                     <option value="">Select Category</option>
                     <option value="Lifestyle">Lifestyle</option>
                     <option value="Tech">Tech</option>
                 </select>
             </div>
-            <label htmlFor="content" className="mb-2">Content: </label>
-            <textarea name = "content" className="h-200 px-2 py-1 mb-6" placeholder="Enter blog content..." onChange={(e) => setContent(e.target.value)}></textarea>
+            <label htmlFor="content" className="mb-4">Content: </label>
+            <textarea name = "content" className="h-100 px-2 py-2 mb-6" placeholder="Enter blog content..." onChange={(e) => setContent(e.target.value)}></textarea>
+            <label htmlFor="title" className="mb-3">About: </label>
+            <input type="text" name="about" value={about} className="px-2 py-2" placeholder="Enter a line or two about your blog..." onChange={(e) => setAbout(e.target.value)}></input>
             <div className="flex justify-end">
-                <button type="submit" className="bg-red-700 text-white py-1 px-2 rounded mb-6">Publish</button>
+                <button type="submit" className="bg-red-700 mt-4 text-white py-1 px-2 rounded mb-6">Publish</button>
             </div>
         </form>
     </div>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -37,27 +37,28 @@ const Dashboard = () => {
     
   return (
     <div className="flex space-x-8 mt-10 px-20">
-      <div className="w-1/3 bg-black text-white rounded-lg">
-        <img src="/Dp1.jpg" className="w-full h-96 object-cover rounded"></img>
+      <div className="w-1/4 bg-black text-white rounded-lg">
+        <img src="/Dp1.jpg" className="w-64 object-cover rounded"></img>
         <h2 className=""></h2>
         <p></p>
         
       </div>
       
-      <div className="w-2/3">
+      <div className="w-3/4">
         <div className="flex justify-end">
           <button className="bg-red-700 text-white py-1 px-2 rounded mb-4 cursor-pointer" onClick={() => navigate('/create')}>+ New Blog</button>
         </div>
         {blogs.map(blog => (
-          <div className="border rounded-lg px-5 py-2" key={blog._id}>
+          <div className="border rounded-lg px-5 py-2 mb-3" key={blog._id}>
             <div className="flex justify-between items-center">
-              <h2 className="py-2 font-bold">{blog.title}</h2>
+              <h2 className="py-2 font-bold truncate max-w-2xl">{blog.title}</h2>
               <div className="space-x-3">
                 <button className="bg-green-600 text-white py-1 px-3 rounded cursor-pointer" onClick={() => handleEdit(blog._id)}>Edit</button>
                 <button className="bg-red-600 text-white py-1 px-3 rounded cursor-pointer" onClick={() => handleDelete(blog._id)}>Delete</button>
               </div>          
             </div>
-            <p>{blog.about}</p>
+            <p className='text-left text-sm text-gray-500'>Category: {blog.category}</p>
+            <p className="line-clamp-3 text-left break-words whitespace-normal">{blog.about}</p>
           </div>
         ))}
       </div>
